5cc11879caa8425f5939506241d7850df4269fc5
smizer ethio <127936304+smizer09@users.noreply.github.com>
2023-03-17 16:52:26 +0000

    added css html pictures dynamic coding bootstrap jsonify

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id':1,
+    'title':'Data analyst',
+    'location':'addis ababa',
+    'salary':'etb. 120,000'
+  },
+  {
+    'id':2,
+    'title':'Data scientist',
+    'location':'addis ababa',
+    'salary':'etb. 130,000'
+  },
+  {
+    'id':3,
+    'title':'front end engineer',
+    'location':'addis ababa',
+    'salary':'etb. 110,000'
+  },
+  {
+    'id':4,
+    'title':'back end engineer',
+    'location':'addis ababa',
+    'salary':'etb. 140,000'
+  }
+]
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return render_template('home.html' , jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
+  
 
 
 if __name__ == "__main__":

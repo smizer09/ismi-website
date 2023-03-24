d1ad5d4328d913bdafe8a8bb3522d1a9be260ebe
smizer ethio <127936304+smizer09@users.noreply.github.com>
2023-03-24 08:30:11 +0000

    dynamic pages

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_DB
+from database import load_jobs_DB, load_job_DB
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    job = load_jobs_DB()
-    return render_template('home.html' , job = job)
+    jobs = load_jobs_DB()
+    return render_template('home.html' , jobs = jobs)
+
+@app.route("/job/<id>")
+def show_jobs(id):
+    job = load_job_DB(id)
+     
+    return render_template('jobpage.html' , job = job)
+    
 
 @app.route("/api/jobs")
 def list_jobs():
   job = load_jobs_DB()
-  return jsonify(job)
+  return jsonify(jobs)
 
   
 
--- database.py
+++ database.py
@@ -11,16 +11,28 @@ def load_jobs_DB():
   
   with engine.connect() as conn:
     result = conn.execute(text("select * from job"))
-    job = []
+    jobs = []
     for row in result.all():
     
       row_as_dict = row._mapping
-      job.append(dict(row_as_dict))
+      jobs.append(dict(row_as_dict))
     
+  return jobs
+
+
+def load_job_DB(id):
+  
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM job WHERE id = {} ".format(id) ))
+    job = []
+    
+
+    for row in result.all():
+      a = row._mapping
+      job.append(dict(a))
   return job
     
-    
   
   
-    
+   
 my_secret = os.environ['DB_CONNECTION_STRING']
